04b861f23bcad75f5a9626c654fe8f41cea6f7b4
tanjaderin <tanja.derin88@gmail.com>
2023-10-27 11:16:39 +0200

    funziona quasi

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
+#include <tuple>
 #include "Sparse_Matrix.hpp"
 
 int main() {
@@ -19,19 +20,45 @@ int main() {
     ptr->print();
     delete ptr;
 
+///matrix prova
     std::vector<std::vector<double>> array1{{1, 0, 3, 5}, 
                                            {9, 7.8, 0, 0},
                                            {10, 0, 0, 0}};
     auto csr2 = SparseMatrixCSR(array1);
+
+//multi prova
     std::vector<double> vsota = csr2*v;
-    std::cout << "Vector elements: ";
+    std::cout << "CSR*v: ";
         for (const double& element : vsota) {
             std::cout << element << ", ";
                 }
         std::cout << std::endl;
-    //std::tuple< std::vector<double>, std::vector<int>, std::vector<int>> coo=csr.to_COO();
+//to COO prova
+    std::tuple< std::vector<double>, std::vector<int>, std::vector<int>> coo=csr2.to_COO();
+    std::cout << "convert to COO: ";
+        std::cout << "values_COO: ";
+        for (const double& element : std::get<0>(coo)) {
+            std::cout << element << ", ";
+                }
+        std::cout << std::endl;
 
+        std::cout << "columns_COO: ";
+        for (const int& element : std::get<1>(coo)) {
+            std::cout << element << ", ";
+                }
+        std::cout << std::endl;
+
+        std::cout << "rows_COO: ";
+        for (const double& element : std::get<2>(coo)) {
+            std::cout << element << ", ";
+                }
+        std::cout << std::endl;
+        //std::cout <<std::get<0>(coo) << ", " std::cout << std::endl;
+        //std::cout <<std::get<1>(coo) << ", " std::cout << std::endl;
+        //std::cout <<std::get<2>(coo) << ", " std::cout << std::endl;
+        
     return 0;
-}
+    }
+
 
 //ref:https://unstop.com/blog/how-to-print-a-vector-in-cpps
